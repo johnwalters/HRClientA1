c4e49224b10d3ed74e229d02582d2acb923c5ef7
John Walters <johnwalters@Mac-c42c0324fa16.local>
2019-03-25 23:18:24 -0700

    typo

--- horse-racing/src/app/local-storage.service.ts
+++ horse-racing/src/app/local-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { LocalStorageCounts } from './LocalStorageCounts';
+import { LocalStorageCounts } from './localStorageCounts';
 
 @Injectable({
   providedIn: 'root'
